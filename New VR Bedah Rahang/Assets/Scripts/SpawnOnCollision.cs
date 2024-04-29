f0d2a1735462e9ceea52868cb1600e5b69cc3c63
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2024-04-29 17:24:36 +0700

    Spawn point by poke done

--- New VR Bedah Rahang/Assets/Scripts/SpawnOnCollision.cs
+++ New VR Bedah Rahang/Assets/Scripts/SpawnOnCollision.cs
@@ -4,29 +4,80 @@ using UnityEngine;
 
 public class SpawnOnCollision : MonoBehaviour
 {
-    public GameObject spherePrefab;
+    public GameObject pointPrefab;
+    public GameObject planePrefab;
+    public LineRenderer lineRenderer;
+
+    private GameObject startPoint;
+    private GameObject endPoint;
+
     private Vector3 contactPoint;
+
     private bool hasCollided = false;
+    private bool canSpawn = true;
+    private float cooldownDuration = 0.5f;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!hasCollided)
+        if (!hasCollided && collision.gameObject.CompareTag("Skull"))
         {
             ContactPoint firstContact = collision.contacts[0];
             contactPoint = firstContact.point;
             hasCollided = true;
-            Debug.Log("Collision Entered");
+            Debug.Log("Collision Entered with Cube");
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (hasCollided)
+        if (hasCollided && canSpawn && collision.gameObject.CompareTag("Skull"))
         {
-            Instantiate(spherePrefab, contactPoint, Quaternion.identity);
-            Debug.Log("Sphere Spawned on Exit");
+            if (startPoint == null)
+            {
+                startPoint = Instantiate(pointPrefab, contactPoint, Quaternion.identity);
+                startPoint.transform.SetParent(collision.gameObject.transform);
+                Debug.Log("Sphere Spawned on Exit as First Point");
+            }
+            else if (endPoint == null)
+            {
+                endPoint = Instantiate(pointPrefab, contactPoint, Quaternion.identity);
+                endPoint.transform.SetParent(collision.gameObject.transform);
+                Debug.Log("Second Point Spawned on Exit as End Point");
 
-            hasCollided = false; // Reset the flag
+                CreateLineBetweenPoints();
+                SpawnPlaneAlongLine();
+
+                startPoint = null;
+                endPoint = null;
+                hasCollided = false;
+                canSpawn = false;
+                Invoke("ResetCooldown", cooldownDuration);
+            }
         }
+    }
+    private void CreateLineBetweenPoints()
+    {
+        if (startPoint != null && endPoint != null && lineRenderer != null)
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, startPoint.transform.position);
+            lineRenderer.SetPosition(1, endPoint.transform.position);
+        }
+    }
+
+    private void SpawnPlaneAlongLine()
+    {
+        if (startPoint != null && endPoint != null && planePrefab != null)
+        {
+            Vector3 planePosition = (startPoint.transform.position + endPoint.transform.position) / 2f;
+            Quaternion planeRotation = Quaternion.LookRotation(endPoint.transform.position - startPoint.transform.position);
+
+            GameObject plane = Instantiate(planePrefab, planePosition, planeRotation);
+            Debug.Log("Plane Spawned along Line");
+        }
+    }
+    private void ResetCooldown()
+    {
+        canSpawn = true;
     }
 }
